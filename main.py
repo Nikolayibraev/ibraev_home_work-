aade4c02b85d2d0a8131a0e306ad071342f5b192
Nikolayibraev <bmterror88@gmail.com>
2024-08-10 20:40:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#1st program
+print((9**0.5)*5)
+
+#2st program
+print(9.99>9.98 and 1000!=100.1)
+
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+#4st program
+x='123.456'
+x=float(x)
+x=int((x*10)%10)
+
+print(x)
