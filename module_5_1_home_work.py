bdd9ba533d371e0ea3ea4390f7e8260501b54c07
Nikolayibraev <bmterror88@gmail.com>
2024-09-27 17:26:51 +0300

    Update module_5_1_home_work.py

--- module_5_1_home_work.py
+++ module_5_1_home_work.py
@@ -5,19 +5,13 @@ class House:
 
     def go_to (self, new_floor):
         for i in range(1,new_floor):
-            print(i)
+            if new_floor <= self.num_of_floor:
+                print(i)
 
         if new_floor > self.num_of_floor or new_floor < 1:
             print('Такого этажа не сущесствует')
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(21)
-h2.go_to(4)
-
-
-
-
-
-
-
+h1.go_to(16)
+h2.go_to(6)
