4c1270c7e0d9d832a6665cd5eadd4b84c6f12a4b
Nikolayibraev <bmterror88@gmail.com>
2024-10-29 01:51:23 +0300

    Add files via upload

---
+++ module_8_home_work.py
@@ -0,0 +1,31 @@
+#--------------- H O M E  W O R K  (M O D U L E _ 8 _ 1) ---------------
+
+# def add_everything_up(a,b):
+#
+#     try:
+#         # если это разные типы данных выполняем конкатенацию
+#             if isinstance(a, float, int) and isinstance(b, float,int):
+#                 return a+b
+#             elif isinstance(a,str) and isinstance(b,str):
+#                 return a+b
+#
+#     except TypeError:
+#         return str(a) + str(b)
+#
+# print(add_everything_up(3, 3))
+# print(add_everything_up('яблоко', 4215))
+# print(add_everything_up(123.456, 7))
+
+
+# -------------- В Т О Р О Й  В А Р И А Н Т ---------------
+
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        return str(a) + str(b)
+
+# Примеры использования функции
+print(add_everything_up(3, 3))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.2, 1.1))
