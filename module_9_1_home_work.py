80f0f29003b05494e16070982b5ccc75ed74fde9
Nikolayibraev <bmterror88@gmail.com>
2024-11-04 20:02:38 +0300

    Add files via upload

---
+++ module_9_1_home_work.py
@@ -0,0 +1,16 @@
+#--------------- H O M E  W O R K  (M O D U L E _ 9 _ 1) ---------------
+
+def apply_all_func(int_list, *functions):
+    results = {}
+
+    for func in functions:
+        func_name = func.__name__
+        results[func_name] = func(int_list)   # создаем ключ и значение
+
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+
