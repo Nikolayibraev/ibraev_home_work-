482486364bc238eb64895e6ba5c491f783bae6ec
Nikolayibraev <bmterror88@gmail.com>
2025-01-02 15:03:22 +0300

    mod_13_5_home_work.py

--- mod_13_5_home_work.py
+++ mod_13_5_home_work.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = '7591312845:AAEMhfVyvpdfSakgRJpiLfnZsXSu0wZMF_4'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
